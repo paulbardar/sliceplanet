fe5b4129b9b417effdfbd8555c854c003868f544
paulbardar <q.c.tulip@gmail.com>
2017-10-17 23:00:18 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,109 @@
+
+
+$(window).load(function(){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+		$('body').addClass('ios');
+	} else{
+		$('body').addClass('web');
+	};
+	$('body').removeClass('loaded');
+});
+/* viewport width */
+function viewport(){
+	var e = window,
+		a = 'inner';
+	if ( !( 'innerWidth' in window ) )
+	{
+		a = 'client';
+		e = document.documentElement || document.body;
+	}
+	return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+};
+/* viewport width */
+$(function(){
+	/* placeholder*/
+	$('input, textarea').each(function(){
+ 		var placeholder = $(this).attr('placeholder');
+ 		$(this).focus(function(){ $(this).attr('placeholder', '');});
+ 		$(this).focusout(function(){
+ 			$(this).attr('placeholder', placeholder);
+ 		});
+ 	});
+	/* placeholder*/
+
+	$('.button-nav').click(function(){
+		$(this).toggleClass('active'),
+		$('.main-nav-list').slideToggle();
+		return false;
+	});
+
+	/* components */
+
+	 if($('.slick-slider').length) {
+    	$('.slick-slider').slick({
+    		dots: true,
+    		infinite: true,
+    		speed: 300,
+    		slidesToShow: 1,
+    		slidesToScroll: 1,
+            prevArrow: '.slider-prev',
+            nextArrow: '.slider-next',
+    		responsive: [
+    			{
+    			  breakpoint: 768,
+    			  settings: {
+    				dots: false
+    			  }
+                }
+    		]
+    	});
+    };
+	/*
+
+	if($('.styled').length) {
+		$('.styled').styler();
+	};
+	if($('.fancybox').length) {
+		$('.fancybox').fancybox({
+			margon  : 10,
+			padding  : 10
+		});
+	}; */
+	/*
+	if($('.scroll').length) {
+		$(".scroll").mCustomScrollbar({
+			axis:"x",
+			theme:"dark-thin",
+			autoExpandScrollbar:true,
+			advanced:{autoExpandHorizontalScroll:true}
+		});
+	};
+
+	*/
+
+	/* components */
+
+
+
+});
+
+var handler = function(){
+
+	var height_footer = $('footer').height();
+	var height_header = $('header').height();
+	//$('.content').css({'padding-bottom':height_footer+40, 'padding-top':height_header+40});
+
+
+	var viewport_wid = viewport().width;
+	var viewport_height = viewport().height;
+
+	if (viewport_wid <= 991) {
+
+	}
+
+}
+$(window).bind('load', handler);
+$(window).bind('resize', handler);
+
+
+
